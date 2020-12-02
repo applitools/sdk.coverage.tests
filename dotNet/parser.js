5e2fc369d9befd2ab15d6e1edc3fd77a77f82209
YuriiChere <56256320+YuriiChere@users.noreply.github.com>
2020-12-02 08:39:58 +0200

    Updates according to updated framework (#12)
    * Updates for updated framework
    
    * Add override-tests file
    
    * emove package-lock.json
    
    * Add package-lock.json
    
    * Fix package command to new framework
    
    * Updates for .CheckFrame
    
    * Remove extra By.CssSelector for frames
    
    * Add support for Firefox, IE, Edge, Safari, Emulators
    
    * Add support for branch name in config
    
    * Add support for headless, hideScrollBars, isDisabled, update CheckRegionInFrame
    
    * Add floating, layout regions, matchLevel. SendDom
    
    * Add ignoreDisplacement, withName
    
    * Add webDriver=eyes.Open
    
    * Remove debugging code, add flag openPerformed
    
    * dotNet/override-tests.js Add blank symbol to test's name 'should set viewport size '
    
    * Add retry for web driver creation, refactoring
    
    * Refactor initialize.js, add 1 test to skip
    
    * Refactor initialize.js
    
    * Simple commit to set lats link

--- dotNet/parser.js
+++ dotNet/parser.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const types = require('./mapping/types')
+const selectors = require('./mapping/selectors')
+
 function checkSettings(cs, mobile=false) {
     let target = `Target`
     if(cs === undefined){
@@ -10,51 +13,167 @@ function checkSettings(cs, mobile=false) {
     if (cs.frames === undefined && cs.region === undefined) element = '.Window()'
     else {
         if (cs.frames) element += frames(cs.frames)
-        if (cs.region) element += region(cs.region, mobile)
+        if (cs.region) {
+			if (cs.region.type) element += region(cs.region, cs.region.type, mobile)
+			else element += region(cs.region, undefined, mobile)
+		}
     }
     if(cs.ignoreRegions) options += ignoreRegions(cs.ignoreRegions, mobile)
+	if (cs.floatingRegions) options += floatingRegions(cs.floatingRegions)
     if(cs.scrollRootElement) options+=scrollRootElement(cs.scrollRootElement)
+	if(cs.accessibilityRegions) options+=accessibilityRegions(cs.accessibilityRegions[0].region, cs.accessibilityRegions[0].type)
+	if(cs.layoutRegions) options+=layoutRegions(cs.layoutRegions)
+	if (cs.ignoreDisplacements !== undefined) options += `.IgnoreDisplacements(${cs.ignoreDisplacements})`
+	if (cs.sendDom !== undefined) options += `.SendDom(${cs.sendDom})`
+    if (cs.matchLevel) options += `.MatchLevel(MatchLevel.${cs.matchLevel})`
     if(cs.isFully) options += '.Fully()'
+	if (cs.name) options += `.WithName(${cs.name})`
     return target + element + options
 }
 
 function frames(arr) {
-    return arr.reduce((acc, val) => acc + `.Frame(\"${getVal(val)}\")`, '')
+	return arr.reduce((acc, val) => acc + `${frame(val)}`, '')
 }
 
-function region(region, mobile=false) {
-    return `.Region(${regionParameter(region, mobile)})`
+function frame(frames) {
+	if (frames && frames.isRef) return `.Frame(` + frames.ref() + `)`
+    switch (typeof frames){
+	case 'object':
+		let stringified = JSON.stringify(frames)
+		if (frames.frame) return `.Frame(${takeSelector(frames.frame)}).ScrollRootElement(By.CssSelector(${takeSelector(frames.scrollRootElement)}))`
+		if (frames.type) return `.Frame(${selectors[frames.type]}(${takeSelector(frames.selector)}))`
+		throw Error(`Couldn't treat frame ${frames} for type 'object'`)
+		break;
+	case 'string':
+		return `.Frame(${takeSelector(frames)})`
+		break;
+	default:
+		throw Error(`Couldn't treat frame ${frames} - code for type ${(typeof frames)} is not ready yet`)
+	}
+}
+
+function takeSelector(selector) {
+	selector = selector.toString()
+    selector = selector.replace(/"/g, "")
+	selector = selector.replace(/'/g, "")
+	selector = selector.replace(/\[/g, "")
+	selector = selector.replace(/\]/g, "")
+	selector = selector.replace('name=', "")
+	selector = '"' + selector + '"'
+	return selector
+}
+
+function region(region) {
+    return `.Region(${regionParameter(region)})`
 }
 
 function ignoreRegions(arr, mobile=false) {
     return arr.reduce((acc, val) => acc + ignore(val, mobile), '')
 }
 
-function ignore(region, mobile=false){
-    return `.Ignore(${regionParameter(region, mobile)})`
+function ignore(region, type=undefined, mobile=false){
+    return `.Ignore(${regionParameter(region, type, mobile)})`
+}
+
+function floatingRegions(arr) {
+	return `.Floating(${regionParameter(arr[0].region)}, ${arr[0].maxUpOffset}, ${arr[0].maxDownOffset}, ${arr[0].maxLeftOffset}, ${arr[0].maxRightOffset})`
+}
+
+function layoutRegions(arr) {
+	return `.Layout(${regionParameter(arr[0])})`
 }
 
 function scrollRootElement(cssSelector) {	
     return `.ScrollRootElement(By.CssSelector(\"${cssSelector}\"))`	
 }
 
-function regionParameter (region, mobile=false) {
+function accessibilityRegions(region, type) {	
+    return `.Accessibility(${regionParameter(region)}, AccessibilityRegionType.${type})`	
+}
+
+function regionParameter (region) {
     let string
     switch (typeof region) {
         case 'string':
-            string = mobile? `Utilities.FindElement(driver, \"${region}\")` : `By.CssSelector(\"${region}\")`
+            string = `By.CssSelector(\"${region}\")`
             break;
         case "object":
-            string = `new Rectangle(${region.left}, ${region.top}, ${region.width}, ${region.height})`
+			if (region.type) string = findElementBySelectorType(region.selector, region.type)
+			else string = `new System.Drawing.Rectangle(${region.left}, ${region.top}, ${region.width}, ${region.height})`
+			break
+		case 'function':
+			string = serializeRegion(region)
+			break
+		default:
+			throw new Error(`Region parameter of the unimplemented type was used`)
     }
     return string
 }
 
-function getVal (val) {
-    let nameAndValue = val.toString().split("\"")
-    return nameAndValue[1]
+function serializeRegion(data) {
+  if (data && data.isRef) {
+    return data.ref()
+  } else if (Array.isArray(data)) {
+    return `[${data.map(serializeRegion).join(', ')}]`
+  } else if (typeof data === 'object' && data !== null) {
+	return `new System.Drawing.Rectangle(${data.left}, ${data.top}, ${data.width}, ${data.height})`
+  } else {
+    return JSON.stringify(data)
+  }
+}
+
+function findElementBySelectorType(selector, type){
+	switch (type) {
+		case 'css':
+            return `By.CssSelector(\"${selector}\")`
+			break;
+		default:
+			return `By.CssSelector(\"${selector}\")`
+	}
+}
+
+function parseAssertActual(actual){
+	let elements = actual.includes('[')? actual.split('[') : actual.split('.');
+	let result = ""
+	elements.forEach(element => {
+		if (result === "") {result = result + element; return;}
+		element = element.replace(/"/g, "")
+		element = element.replace(/]/g, "")
+		if (isNaN(Number(element))) element = "." + element.charAt(0).toUpperCase() + element.slice(1)
+		else element = "[" + element + "]"
+		if (element === ".Version") element = ".GuidelinesVersion"
+		result = result + element
+	})
+	return result
+}
+
+function expectParser(expected){
+	if (expected.hasOwnProperty('left')){
+		if (expected.hasOwnProperty('maxUpOffset')) return `new FloatingMatchSettings(${expected.left}, ${expected.top}, ${expected.width}, ${expected.height}, ${expected.maxUpOffset}, ${expected.maxDownOffset}, ${expected.maxLeftOffset}, ${expected.maxRightOffset})`
+		else {
+			if (expected.hasOwnProperty('isDisabled')) return `new AccessibilityRegionByRectangle(${expected.left}, ${expected.top}, ${expected.width}, ${expected.height}, AccessibilityRegionType.${expected.type})`
+			else return `new Region(${expected.left}, ${expected.top}, ${expected.width}, ${expected.height})`//return `new Rectangle(${expected.left}, ${expected.top}, ${expected.width}, ${expected.height})`
+		}
+	}
+	else{ 
+		switch (expected){
+			case 'AA':
+			case 'AAA':
+				return `AccessibilityLevel.` + expected
+			case 'WCAG_2_0':
+			case 'WCAG_2_1':
+				return `AccessibilityGuidelinesVersion.` + expected
+		}
+		if (expected.hasOwnProperty('width')) return `new RectangleSize(${expected.width}, ${expected.height})`
+		if (expected.hasOwnProperty('applitools_title')) return `new Region(${expected.applitools_title[0].left}, ${expected.applitools_title[0].top}, ${expected.applitools_title[0].width}, ${expected.applitools_title[0].height})`
+		return expected
+	}
 }
 
 module.exports = {
-    checkSettingsParser: checkSettings
+    checkSettingsParser: checkSettings,
+	regionParameterParser: regionParameter,
+	parseAssertActual: parseAssertActual,
+	expectParser: expectParser,
+	takeSelector: takeSelector,
 }
--- dotNet/test/Selenium/TestSetupGenerated.cs
+++ dotNet/test/Selenium/TestSetupGenerated.cs
@@ -2,9 +2,17 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
+using System.Threading;
 using OpenQA.Selenium.Chrome;
 using Applitools.Selenium;
 using Applitools.Tests.Utils;
+using Applitools.Utils.Geometry;
+using System.Collections.Generic;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Remote;
 
 namespace Applitools.Generated.Selenium.Tests
 {
@@ -12,35 +20,254 @@ namespace Applitools.Generated.Selenium.Tests
     {
 
         protected IWebDriver driver;
+        protected IWebDriver webDriver;
         protected EyesRunner runner;
         protected Eyes eyes;
         protected string testedPageUrl = "https://applitools.github.io/demo/TestPages/FramesTestPage/";
         public static readonly BatchInfo BatchInfo = new BatchInfo("DotNet Generated Tests");
-		public static readonly string DRIVER_PATH = Environment.GetEnvironmentVariable("DRIVER_PATH");
-
-        [SetUp]
-        public void SetUpSelenium()
+		public static readonly string DRIVER_PATH = Environment.GetEnvironmentVariable("DRIVER_PATH"); 
+        public static readonly string SAUCE_USERNAME = Environment.GetEnvironmentVariable("SAUCE_USERNAME");
+        public static readonly string SAUCE_ACCESS_KEY = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
+        public static readonly string SAUCE_SELENIUM_URL = "https://ondemand.saucelabs.com:443/wd/hub";
+        protected enum browserType
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--headless");
-            driver = DRIVER_PATH != null ? new ChromeDriver(DRIVER_PATH, options) : new ChromeDriver(options);
-            driver.Navigate().GoToUrl(testedPageUrl);
+            Chrome,
+            IE,
+            Edge,
+            Firefox,
+            Safari11,
+            Safari12
+        }
+        protected DriverOptions options_;
+        private SafariOptions browserOptions;
+        private DesiredCapabilities caps;
+
+        protected void SetUpDriver(browserType browser = browserType.Chrome, bool legacy = false, bool headless = false)
+        {
+            switch(browser)
+            {
+                case browserType.Chrome:
+                    driver = CreateChromeDriver(headless: headless);
+                    break;
+                case browserType.Firefox:
+                    driver = CreateFirefoxDriver(headless: headless);
+                    break;
+                case browserType.IE:
+                    var sauceOptions = new Dictionary<string, object>();
+                    sauceOptions.Add("username", SAUCE_USERNAME);
+                    sauceOptions.Add("accesskey", SAUCE_ACCESS_KEY);
+                    var browserOptionsIE = new InternetExplorerOptions();
+                    browserOptionsIE.PlatformName = "Windows 10";
+                    browserOptionsIE.BrowserVersion = "11.285";
+                    browserOptionsIE.AddAdditionalCapability("sauce:options", sauceOptions, true);
+                    driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), browserOptionsIE.ToCapabilities(), TimeSpan.FromMinutes(4));
+                    break;
+                case browserType.Edge:
+                    var sauceOptionsEdge = new Dictionary<string, object>();
+                    sauceOptionsEdge.Add("username", SAUCE_USERNAME);
+                    sauceOptionsEdge.Add("accesskey", SAUCE_ACCESS_KEY);
+                    var browserOptionsEdge = new EdgeOptions();
+                    browserOptionsEdge.PlatformName = "Windows 10";
+                    browserOptionsEdge.BrowserVersion = "18.17763";
+                    browserOptionsEdge.AddAdditionalCapability("sauce:options", sauceOptionsEdge);
+                    driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), browserOptionsEdge.ToCapabilities(), TimeSpan.FromMinutes(4));
+                    break;
+                case browserType.Safari11:
+                    if (legacy)
+                    {
+                        setDesiredCapabilities("macOS 10.12", "Safari", "11.0");
+                        driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), caps, TimeSpan.FromMinutes(4));
+                    }
+                    else
+                    {
+                        browserOptions = new SafariOptions();
+                        setDriverOptions(ref browserOptions, "macOS 10.12", "11.0");
+                        driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), browserOptions.ToCapabilities(), TimeSpan.FromMinutes(4));
+                    }
+                    break;
+                case browserType.Safari12:
+                    if (legacy)
+                    {
+                        setDesiredCapabilities("macOS 10.13", "Safari", "12.1");
+                        driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), caps, TimeSpan.FromMinutes(4));
+                    }
+                    else
+                    {
+                        browserOptions = new SafariOptions();
+                        setDriverOptions(ref browserOptions, "macOS 10.13", "12.1");
+                        driver = new RemoteWebDriver(new Uri(SAUCE_SELENIUM_URL), browserOptions.ToCapabilities(), TimeSpan.FromMinutes(4));
+                    }
+                    break;
+                default:
+                    throw new Exception("Unknown browser type");
+            }
+        }
+
+
+        private void setDriverOptions(ref SafariOptions driverOptions, string PlatformName, string BrowserVersion)
+        {
+            var sauceOptions = new Dictionary<string, object>();
+            sauceOptions.Add("username", SAUCE_USERNAME);
+            sauceOptions.Add("accesskey", SAUCE_ACCESS_KEY);
+            driverOptions.PlatformName = PlatformName;
+            driverOptions.BrowserVersion = BrowserVersion;
+            driverOptions.AddAdditionalCapability("username", SAUCE_USERNAME);
+            driverOptions.AddAdditionalCapability("accesskey", SAUCE_ACCESS_KEY);
+        }
+
+        private void setDesiredCapabilities(string PlatformName, string BrowserName, string BrowserVersion)
+        {
+            caps = new DesiredCapabilities();
+            caps.SetCapability("browserName", BrowserName);
+            caps.SetCapability("platform", PlatformName);
+            caps.SetCapability("version", BrowserVersion);
+            caps.SetCapability("seleniumVersion", "3.4.0");
+            caps.SetCapability("username", SAUCE_USERNAME);
+            caps.SetCapability("accesskey", SAUCE_ACCESS_KEY);
+        }
+
+        protected static ChromeDriver CreateChromeDriver(ChromeOptions options = null, bool headless = false)
+        {
+            ChromeDriver webDriver = RetryCreateWebDriver(() =>
+            {
+                if (options == null)
+                {
+                    options = new ChromeOptions();
+                }
+                if (headless) options.AddArgument("--headless");
+
+                ChromeDriver webDriverRet = DRIVER_PATH != null ? new ChromeDriver(DRIVER_PATH, options, TimeSpan.FromMinutes(4)) : new ChromeDriver(options);
+                return webDriverRet;
+            });
+            return webDriver;
+        }
+
+        protected static FirefoxDriver CreateFirefoxDriver(bool headless = false)
+        {
+            FirefoxDriver webDriver = RetryCreateWebDriver(() =>
+            {
+                var options = new FirefoxOptions();
+                if (headless) options.AddArgument("--headless");
+                FirefoxDriver webDriverRet = new FirefoxDriver(DRIVER_PATH, options);
+                return webDriverRet;
+            });
+            return webDriver;
         }
 
         protected void initEyes(bool isVisualGrid, bool isCSSMode)
         {
             runner = isVisualGrid ? (EyesRunner)(new VisualGridRunner(10)) : new ClassicRunner();
             eyes = new Eyes(runner);
-            eyes.HostOS = "Linux";
+            initEyesSettings(isVisualGrid, isCSSMode);
+        }
+
+        protected void initEyesSettings(bool isVisualGrid, bool isCSSMode)
+        {
             eyes.Batch = BatchInfo;
             if (!isVisualGrid) eyes.StitchMode = isCSSMode ? StitchModes.CSS : StitchModes.Scroll;
             eyes.BranchName = "master";
             eyes.ParentBranchName = "master";
-			eyes.SaveNewTests = false;
+            eyes.SaveNewTests = false;
             //eyes.AddProperty("ForceFPS", eyes.ForceFullPageScreenshot ? "true" : "false");
             //eyes.AddProperty("Agent ID", eyes.FullAgentId);
-			//eyes.HideScrollbars = true;
+            eyes.HideScrollbars = true;
         }
 
+
+        protected bool isStaleElementError(Exception errorObj)
+		{
+			return (errorObj is StaleElementReferenceException);
+		}
+
+        protected void compareProcedure(Region actualRegion, Region expectedRegion, string type=null)
+        {
+            Region[] actualRegions = new Region[] { actualRegion };
+            HashSet<Region> expectedRegions = new HashSet<Region> { expectedRegion };
+            TestUtils.CompareSimpleRegionsList_(actualRegions, expectedRegions, "Region");
+        }
+
+        protected void compareProcedure(Dictionary<string, object> actualRegion, RectangleSize expectedRegion, string type)
+        {
+            RectangleSize actual = new RectangleSize((int)(long)actualRegion["width"], (int)(long)actualRegion["height"]);
+            Assert.IsTrue(RectangleSize.AreEqual(expectedRegion, actual), "Actual region with Width="+ actual.Width.ToString()+" Height="+ actual.Height.ToString()+ " don't equal to expected region with Width = "+ expectedRegion.Width.ToString()+" Height = "+ expectedRegion.Height.ToString());
+        }
+
+        protected void compareProcedure(int actual, object expected, string type = null)
+        {
+            Assert.AreEqual((int)expected, actual);
+        }
+
+        protected void compareProcedure(FloatingMatchSettings actualRegion, FloatingMatchSettings expectedRegion, string type= "Floating")
+        {
+            FloatingMatchSettings[] actualRegions = new FloatingMatchSettings[] { actualRegion };
+            HashSet<FloatingMatchSettings> expectedRegions = new HashSet<FloatingMatchSettings> { expectedRegion };
+            HashSet<FloatingMatchSettings> expectedRegionsClone = new HashSet<FloatingMatchSettings>(expectedRegions);
+            if (expectedRegions.Count > 0)
+            {
+                foreach (FloatingMatchSettings region in actualRegions)
+                {
+                    if (!expectedRegionsClone.Remove(region))
+                    {
+                        Assert.Fail("actual {0} region {1} not found in expected regions list", type, region);
+                    }
+                }
+                Assert.IsEmpty(expectedRegionsClone, "not all expected regions found in actual regions list.", type);
+            }
+        }
+
+        protected void compareProcedure(AccessibilityRegionByRectangle actualRegion, AccessibilityRegionByRectangle expectedRegion, string type="")
+        {
+            AccessibilityRegionByRectangle[] actualRegions = new AccessibilityRegionByRectangle[] { actualRegion };
+            HashSet<AccessibilityRegionByRectangle> expectedRegions = new HashSet<AccessibilityRegionByRectangle> { expectedRegion };
+            HashSet<AccessibilityRegionByRectangle> expectedRegionsClone = new HashSet<AccessibilityRegionByRectangle>(expectedRegions);
+            if (expectedRegions.Count > 0)
+            {
+                foreach (AccessibilityRegionByRectangle region in actualRegions)
+                {
+                    if (!expectedRegionsClone.Remove(region))
+                    {
+                        Assert.Fail("actual {0} region {1} not found in expected regions list", type, region);
+                    }
+                }
+                Assert.IsEmpty(expectedRegionsClone, "not all expected regions found in actual regions list.", type);
+            }
+        }
+
+        protected void compareProcedure(AccessibilityLevel actualLevel, AccessibilityLevel expectedLevel, string type=null)
+        {
+            Assert.IsTrue(actualLevel == expectedLevel, "Actual Level '"+actualLevel.ToString()+"' don't equal to Expected Level '" + expectedLevel.ToString() + "'");
+        }
+
+        protected void compareProcedure(AccessibilityGuidelinesVersion actualVersion, AccessibilityGuidelinesVersion expectedVersion, string type = null)
+        {
+            Assert.IsTrue(actualVersion == expectedVersion, "Actual Version '" + actualVersion.ToString() + "' don't equal to Expected Version '" + expectedVersion.ToString() + "'");
+        }
+
+        protected void compareProcedure(Boolean actual, Boolean expected, string type = null)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static T RetryCreateWebDriver<T>(Func<T> func, int times = 3) where T : class, IWebDriver
+        {
+            int retriesLeft = times;
+            int wait = 500;
+            while (retriesLeft-- > 0)
+            {
+                try
+                {
+                    T result = func.Invoke();
+                    if (result != null) return result;
+                }
+                catch (Exception)
+                {
+                    if (retriesLeft == 0) throw;
+                }
+                Thread.Sleep(wait);
+                wait *= 2;
+                wait = Math.Min(10000, wait);
+            }
+            return null;
+        }
     }
 }
