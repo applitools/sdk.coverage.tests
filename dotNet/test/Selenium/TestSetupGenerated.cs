d4f6bc940b280b2ff6212b6954888af6fbc2947c
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-12-17 14:51:54 +0200

    Bugfix trello 2379 (#21)
    * added a method to TestSetupGenerated.
    
    * Parse the DOM string to a JSON object.
    
    * Add support for getDom (#20)
    
    * Add support for getDom
    
    * Fix for number indexes
    
    Co-authored-by: YuriiChere <56256320+YuriiChere@users.noreply.github.com>

--- dotNet/test/Selenium/TestSetupGenerated.cs
+++ dotNet/test/Selenium/TestSetupGenerated.cs
@@ -13,6 +13,10 @@ using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Safari;
 using OpenQA.Selenium.Remote;
+using System.Web;
+using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Applitools.Generated.Selenium.Tests
 {
@@ -25,7 +29,7 @@ namespace Applitools.Generated.Selenium.Tests
         protected Eyes eyes;
         protected string testedPageUrl = "https://applitools.github.io/demo/TestPages/FramesTestPage/";
         public static readonly BatchInfo BatchInfo = new BatchInfo("DotNet Generated Tests");
-		public static readonly string DRIVER_PATH = Environment.GetEnvironmentVariable("DRIVER_PATH"); 
+        public static readonly string DRIVER_PATH = Environment.GetEnvironmentVariable("DRIVER_PATH");
         public static readonly string SAUCE_USERNAME = Environment.GetEnvironmentVariable("SAUCE_USERNAME");
         public static readonly string SAUCE_ACCESS_KEY = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
         public static readonly string SAUCE_SELENIUM_URL = "https://ondemand.saucelabs.com:443/wd/hub";
@@ -44,7 +48,7 @@ namespace Applitools.Generated.Selenium.Tests
 
         protected void SetUpDriver(browserType browser = browserType.Chrome, bool legacy = false, bool headless = false)
         {
-            switch(browser)
+            switch (browser)
             {
                 case browserType.Chrome:
                     driver = CreateChromeDriver(headless: headless);
@@ -175,11 +179,11 @@ namespace Applitools.Generated.Selenium.Tests
 
 
         protected bool isStaleElementError(Exception errorObj)
-		{
-			return (errorObj is StaleElementReferenceException);
-		}
+        {
+            return (errorObj is StaleElementReferenceException);
+        }
 
-        protected void compareProcedure(Region actualRegion, Region expectedRegion, string type=null)
+        protected void compareProcedure(Region actualRegion, Region expectedRegion, string type = null)
         {
             Region[] actualRegions = new Region[] { actualRegion };
             HashSet<Region> expectedRegions = new HashSet<Region> { expectedRegion };
@@ -193,7 +197,7 @@ namespace Applitools.Generated.Selenium.Tests
         protected void compareProcedure(Dictionary<string, object> actualRegion, RectangleSize expectedRegion, string type)
         {
             RectangleSize actual = new RectangleSize((int)(long)actualRegion["width"], (int)(long)actualRegion["height"]);
-            Assert.IsTrue(RectangleSize.AreEqual(expectedRegion, actual), "Actual region with Width="+ actual.Width.ToString()+" Height="+ actual.Height.ToString()+ " don't equal to expected region with Width = "+ expectedRegion.Width.ToString()+" Height = "+ expectedRegion.Height.ToString());
+            Assert.IsTrue(RectangleSize.AreEqual(expectedRegion, actual), "Actual region with Width=" + actual.Width.ToString() + " Height=" + actual.Height.ToString() + " don't equal to expected region with Width = " + expectedRegion.Width.ToString() + " Height = " + expectedRegion.Height.ToString());
         }
 
         protected void compareProcedure(int actual, object expected, string type = null)
@@ -201,7 +205,7 @@ namespace Applitools.Generated.Selenium.Tests
             Assert.AreEqual((int)expected, actual);
         }
 
-        protected void compareProcedure(FloatingMatchSettings actualRegion, FloatingMatchSettings expectedRegion, string type= "Floating")
+        protected void compareProcedure(FloatingMatchSettings actualRegion, FloatingMatchSettings expectedRegion, string type = "Floating")
         {
             FloatingMatchSettings[] actualRegions = new FloatingMatchSettings[] { actualRegion };
             HashSet<FloatingMatchSettings> expectedRegions = new HashSet<FloatingMatchSettings> { expectedRegion };
@@ -219,7 +223,7 @@ namespace Applitools.Generated.Selenium.Tests
             }
         }
 
-        protected void compareProcedure(AccessibilityRegionByRectangle actualRegion, AccessibilityRegionByRectangle expectedRegion, string type="")
+        protected void compareProcedure(AccessibilityRegionByRectangle actualRegion, AccessibilityRegionByRectangle expectedRegion, string type = "")
         {
             AccessibilityRegionByRectangle[] actualRegions = new AccessibilityRegionByRectangle[] { actualRegion };
             HashSet<AccessibilityRegionByRectangle> expectedRegions = new HashSet<AccessibilityRegionByRectangle> { expectedRegion };
@@ -237,9 +241,9 @@ namespace Applitools.Generated.Selenium.Tests
             }
         }
 
-        protected void compareProcedure(AccessibilityLevel actualLevel, AccessibilityLevel expectedLevel, string type=null)
+        protected void compareProcedure(AccessibilityLevel actualLevel, AccessibilityLevel expectedLevel, string type = null)
         {
-            Assert.IsTrue(actualLevel == expectedLevel, "Actual Level '"+actualLevel.ToString()+"' don't equal to Expected Level '" + expectedLevel.ToString() + "'");
+            Assert.IsTrue(actualLevel == expectedLevel, "Actual Level '" + actualLevel.ToString() + "' don't equal to Expected Level '" + expectedLevel.ToString() + "'");
         }
 
         protected void compareProcedure(AccessibilityGuidelinesVersion actualVersion, AccessibilityGuidelinesVersion expectedVersion, string type = null)
@@ -250,6 +254,12 @@ namespace Applitools.Generated.Selenium.Tests
         protected void compareProcedure(Boolean actual, Boolean expected, string type = null)
         {
             Assert.AreEqual(expected, actual);
+        }
+
+        protected JObject getDom(TestResults results, string domId)
+        {
+            string dom = TestUtils.GetDom(Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY_READ"), results, domId);
+            return JObject.Parse(dom);
         }
 
         public static T RetryCreateWebDriver<T>(Func<T> func, int times = 3) where T : class, IWebDriver
