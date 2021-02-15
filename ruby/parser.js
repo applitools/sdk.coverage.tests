ea63127977923279bf2c454aa6e72d00b906e97c
artem <artem.borodavka@applitools.com>
2021-02-15 17:06:20 +0200

    update serialize

--- ruby/parser.js
+++ ruby/parser.js
@@ -161,7 +161,7 @@ function serialize(value) {
     } else if (typeof value === 'undefined' || value === null) {
         stringified = 'nil'
     } else if (typeof value === 'string') {
-        stringified = `'${value}'`
+        stringified = `'${JSON.stringify(value)}'`
     } else if (isSelector(value)) {
         stringified = selectors[value.type](value.selector)
     } else if (typeof value === 'object') {
