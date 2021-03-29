730a1d32137c2235043eb1b8165db3d5bf55696e
artem <artem.borodavka@applitools.com>
2021-03-29 12:19:19 +0300

    add typings

--- coverage-tests.js
+++ coverage-tests.js
@@ -122,10 +122,10 @@ test('check window with layout breakpoints', {
   },
   test({eyes, driver, assert}) {
     eyes.open({appName: 'Applitools Eyes SDK'})
-    const expectedViewportSize = driver.executeScript('return {width: window.innerWidth, height: window.innerHeight}')
+    const expectedViewportSize = driver.executeScript('return {width: window.innerWidth, height: window.innerHeight}').type('Map<String, Number>')
     eyes.check({layoutBreakpoints: [500, 1000]})
     eyes.close()
-    const actualViewportSize = driver.executeScript('return {width: window.innerWidth, height: window.innerHeight}')
+    const actualViewportSize = driver.executeScript('return {width: window.innerWidth, height: window.innerHeight}').type('Map<String, Number>')
     assert.equal(actualViewportSize, expectedViewportSize)
   }
 })
