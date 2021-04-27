17c4e593855823940fa9d864898617b049d76a6f
artem <artem.borodavka@applitools.com>
2021-04-27 13:18:19 +0300

    unskip custom batch properties for ruby

--- ruby/overrides.js
+++ ruby/overrides.js
@@ -167,7 +167,6 @@ module.exports = {
     'check window fully with html scrollRootElement after scroll when fail to scroll with css stitching': {skipEmit: true},
     'should not fail if scroll root is stale on android': {skipEmit: true},
     'check region by selector in frame fully on firefox legacy': { skipEmit: true },
-    'should send custom batch properties': {skipEmit: true},
     'adopted styleSheets on chrome': {skipEmit: true},
 	'adopted styleSheets on firefox': {skipEmit: true},
     'variant id': {skipEmit: true},
