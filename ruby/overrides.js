11998a5f4cebab6352e9f6cb52cb18b18d892d4b
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-04-29 08:37:17 +0300

    Remove skip for 'check window fully on android chrome emulator...' 3 tests

--- ruby/overrides.js
+++ ruby/overrides.js
@@ -66,11 +66,12 @@ module.exports = {
     'should set viewport size': {skip: true},
     'should set viewport size on edge legacy': {skip: true},
     // Hover errors
+    'check hovered region by element with css stitching': {skip: true}, // Error: Applitools::OutOfBoundsException: Region (0, 0), 0 x 0 is out of screenshot bounds.
     'check hovered region by element with scroll stitching': {skip: true}, // Diff, hovered element was checked without hovering
     // Chrome emulator have wrong type of browser used
-    'check window fully on android chrome emulator on mobile page': {skipEmit: true},
-    'check window fully on android chrome emulator on mobile page with horizontal scroll': {skipEmit: true},
-    'check window fully on android chrome emulator on desktop page': {skipEmit: true},
+    //'check window fully on android chrome emulator on mobile page': {skipEmit: true},
+    //'check window fully on android chrome emulator on mobile page with horizontal scroll': {skipEmit: true},
+    //'check window fully on android chrome emulator on desktop page': {skipEmit: true},
 
     // Diffs
     'acme login with css stitching': {skip: true},
