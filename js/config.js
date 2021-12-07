8d213cc8fde42ee06f748e4b0ee0009c42fdead7
artem <artem.borodavka@applitools.com>
2021-12-07 14:56:25 +0200

    update js config to point to the universal-sdk branch

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 module.exports = {
   output: './test/generic',
   ext: '.spec.js',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/emitter.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/emitter.js',
   overrides: [
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/overrides.js',
-    ...(process.env.APPLITOOLS_TEST_REMOTE === 'eg' ? ['https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/eg.overrides.js'] : []),
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
+    ...(process.env.APPLITOOLS_TEST_REMOTE === 'eg' ? ['https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/eg.overrides.js'] : []),
   ],
   template:
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/template.hbs',
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/template.hbs',
+  tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
   format: {
     parser: 'babel',
     singleQuote: true,
