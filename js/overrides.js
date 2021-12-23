11405757b2813483558bd12d72b0e1714870ac50
kyrylo <kyrylo.onufriiev@applitools.com>
2021-12-23 15:05:44 +0200

    add two new overrides

--- js/overrides.js
+++ js/overrides.js
@@ -1,16 +1,6 @@
 module.exports = {
-    /**
-     *  Fails because of the difference between SCREEN SIZE and LAYOUT VIEWPORT SIZE.
-     *  We should use screen.width/screen.height for cropping/scaling and innerWidth/innerHeight to understand element location related to the viewport
-     */
-    // 'check region in frame hidden under top bar fully with css stitching': {skip: true},
-    // 'check region in frame hidden under top bar fully with scroll stitching': {skip: true},
-
     // Test have diffs after JS update
-    // 'check window fully on page with sticky header with scroll stitching': {skip: true},
     // 'check window fully on android chrome emulator on desktop page': {skip: true},
-    // 'check region by selector fully on page with sticky header with scroll stitching': {skip: true},
-
     /// Updating baselines to JS
     // Window
     'check window two times with vg classic': {config: {branchName: 'universal-sdk'}}, // was skipped
@@ -20,6 +10,7 @@ module.exports = {
     'check window fully and frame in frame fully with scroll stitching': {config: {branchName: 'universal-sdk'}}, // was v2
     'check window fully and frame in frame fully with vg': {config: {branchName: 'universal-sdk'}}, // was v2
     'check window fully with custom scroll root with css stitching': {config: {branchName: 'universal-sdk'}}, // was v2
+    'check window fully on page with sticky header with scroll stitching': {config: {branchName: 'universal-sdk'}}, // different overlap sizes and mechanism
     // Region
     'check region by coordinates in frame with css stitching': {config: {branchName: 'universal-sdk'}}, // was skipped
     'check region by coordinates in frame with scroll stitching': {config: {branchName: 'universal-sdk'}}, // was skipped
@@ -27,6 +18,7 @@ module.exports = {
     'check overflowed region by coordinates with scroll stitching': {config: {branchName: 'universal-sdk'}}, // was skipped
     'check regions by coordinates in overflowed frame with scroll stitching': {config: {branchName: 'universal-sdk'}}, // was next
     'check region by selector with vg classic': {config: {branchName: 'universal-sdk'}}, // was no-fully-by-default
+    'check region by selector fully on page with sticky header with scroll stitching': {config: {branchName: 'universal-sdk'}}, // different overlap sizes and mechanism
     // Frame
     'check frame in frame fully with css stitching': {config: {branchName: 'universal-sdk'}}, // was v1
     'check frame in frame fully with scroll stitching': {config: {branchName: 'universal-sdk'}}, // was v1
