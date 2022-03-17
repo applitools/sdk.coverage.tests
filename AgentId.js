aef6d510fcd1ed3e630b4bad77c3d1f807ed7757
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2022-03-17 18:24:13 +0200

    Add test 'should send Agent Id'

---
+++ AgentId.js
@@ -0,0 +1,5 @@
+{
+    Eyes.Sdk.DotNet : "Eyes.Selenium4.DotNet/2.59",
+    eyes.sdk.java3 : "eyes-universal/eyes.sdk.java/2.0.0/5.0.2",
+    eyes.sdk.python : "eyes-universal/eyes.sdk.python/2.0.1/5.1.0"
+}
