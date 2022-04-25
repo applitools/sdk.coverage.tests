e688be54d6a514bfbc2a8fbf44b5de65341a8367
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-04-26 00:05:58 +0200

    Update python overrides
    Restore check window on mobile web android test

--- python/overrides.js
+++ python/overrides.js
@@ -9,10 +9,6 @@ module.exports = {
     'should handle check of stale element if selector is preserved': {skip: true},
     'should handle check of stale element in frame if selector is preserved': {skip: true},
 
-    // Failing on the universal server 1.10 and lower
-    // check on new universal server version
-    "check window on mobile web android": {skip: true},
-
     // TODO verify and enable
     "appium android landscape mode check window on android 7": {skipEmit: true},
     "appium android landscape mode check window on android 10": {skipEmit: true},
