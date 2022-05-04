8bbcf7e95a89bce194e3fd86ce2820cd2e950023
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-05-04 17:31:10 +0200

    Enable appium android landscape mode checks (#154)

--- python/overrides.js
+++ python/overrides.js
@@ -8,10 +8,4 @@ module.exports = {
     // Stale element are not handled by python binding
     'should handle check of stale element if selector is preserved': {skip: true},
     'should handle check of stale element in frame if selector is preserved': {skip: true},
-
-    // TODO verify and enable
-    "appium android landscape mode check window on android 7": {skipEmit: true},
-    "appium android landscape mode check window on android 10": {skipEmit: true},
-    "appium android landscape mode check region on android 7": {skipEmit: true},
-    "appium android landscape mode check region on android 10": {skipEmit: true},
 }
