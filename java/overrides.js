26d3961596916099878047b24fe605f690f4a05f
Chaim Aharonson <chaim.aharonson@applitools.com>
2022-06-19 10:34:37 +0300

    update overrides

--- java/overrides.js
+++ java/overrides.js
@@ -79,6 +79,6 @@ module.exports = {
     'lazy load page with default options': {skipEmit: true},
     'lazy load page with default options with vg': {skipEmit: true},
 
-    'should use regions padding': { skipEmit: true },
-    'should use regions padding with vg': { skipEmit: true },
+    "should use regions padding": {skipEmit: true},
+    "should use regions padding with vg": {skipEmit: true},
 }
--- python/overrides.js
+++ python/overrides.js
@@ -7,4 +7,7 @@ module.exports = {
     'should handle check of stale element in frame if selector is preserved': {skip: true},
 
     "Should return exception in TestResultsSummary": {skipEmit: true},
+    
+    "should use regions padding": {skipEmit: true},
+    "should use regions padding with vg": {skipEmit: true},
 }
