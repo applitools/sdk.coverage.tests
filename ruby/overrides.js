a27b594d1844426120dc252699f3f6f412b71eb7
Dave Haeffner <david.haeffner@applitools.com>
2022-06-30 13:35:27 -0400

    Apply suggestions from code review
    🙄
    
    Co-authored-by: Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>

--- ruby/overrides.js
+++ ruby/overrides.js
@@ -26,8 +26,8 @@ module.exports = {
     "should use regions padding": {skipEmit: true},
     "should use regions padding with vg": {skipEmit: true},
 
-    'regionId can be specified as part of checkSettings classic': {skipEmit: true},
-    'regionId can be specified as part of checkSettings with vg': {skipEmit: true},
-    'coded regions with selectors or elements should automatically include a regionId classic': {skipEmit: true},
-    'coded regions with selectors or elements should automatically include a regionId with vg': {skipEmit: true},
+    "regionId can be specified as part of checkSettings classic": {skipEmit: true},
+    "regionId can be specified as part of checkSettings with vg": {skipEmit: true},
+    "coded regions with selectors or elements should automatically include a regionId classic": {skipEmit: true},
+    "coded regions with selectors or elements should automatically include a regionId with vg": {skipEmit: true},
 }
