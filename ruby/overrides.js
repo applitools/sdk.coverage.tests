fb612bb48841a4aef6ead2c6a1b77b675dcdb1f6
Dave Haeffner <david.haeffner@applitools.com>
2022-09-28 13:33:10 -0400

    Update ruby/overrides.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- ruby/overrides.js
+++ ruby/overrides.js
@@ -25,5 +25,5 @@ module.exports = {
    
     // need to add support for the webview property in check settings to the emitter
     "should capture webview when specified in check settings on ios": {skipEmit: true},
-    'should capture webview when specified in check settings on android': {skipEmit: true}
+    "should capture webview when specified in check settings on android": {skipEmit: true}
 }
