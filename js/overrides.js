9daddddc11a5755770a6b30cb3c8db06874f3d59
Dave Haeffner <david.haeffner@applitools.com>
2022-09-29 11:12:14 -0400

    skip test due to inability to get session started when running test
    e.g., https://github.com/applitools/eyes.sdk.javascript1/actions/runs/3141379887/jobs/5103756080

--- js/overrides.js
+++ js/overrides.js
@@ -35,4 +35,7 @@ module.exports = {
     'should send ignore region by selector with vg': {config: {branchName: 'universal-sdk'}}, // was skipped
     'should send floating region by coordinates with vg': {config: {branchName: 'universal-sdk'}}, // was skipped
     'should send multiple accessibility regions by selector with vg': {config: {branchName: 'universal-sdk'}}, // was v1
+    // skip due to inabilty to get session started when running test
+    // e.g., https://github.com/applitools/eyes.sdk.javascript1/actions/runs/3141379887/jobs/5103756080
+    'should send dom': {skip: true},
 }
