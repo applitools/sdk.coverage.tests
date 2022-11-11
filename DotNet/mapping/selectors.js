efe338c2f88028847c0e1fd33d5ecc396487c2ef
artem <artem.borodavka@applitools.com>
2022-11-11 19:51:47 +0200

    update dotnet selectors

--- DotNet/mapping/selectors.js
+++ DotNet/mapping/selectors.js
@@ -2,6 +2,7 @@ module.exports = {
     "css": `By.CssSelector`,
 	"class name": `By.ClassName`,
 	"id": `By.Id`,
+	"xpath": 'By.XPath',
 	"accessibility id": `MobileBy.AccessibilityId`,
 	"-android uiautomator": `MobileBy.AndroidUIAutomator`,
 	"androidViewTag": `MobileBy.AndroidViewTag`,
