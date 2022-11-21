e0d38522b54ba04165d2aab1cc5dabeb4a6e0b2b
at-ylazarenko <89515968+at-ylazarenko@users.noreply.github.com>
2022-11-21 18:06:50 +0200

    Update overrides.js
    Skipped test "should find regions by visual locator" - missing emitter for locator

--- DotNet/overrides.js
+++ DotNet/overrides.js
@@ -29,6 +29,9 @@ module.exports = {
     "pageCoverage data is correct with vg": {skipEmit: true},
     "appium iOS check fully window with scroll and pageCoverage": {skipEmit: true},
     "appium iOS check window region with scroll and pageCoverage": {skipEmit: true},
+    
+    // locator emitter
+    "should find regions by visual locator": {skipEmit: true},
   
     // Flaky tests (waitBeforeCapture isn't implemented)
     'should waitBeforeCapture in open': {skip: true},
