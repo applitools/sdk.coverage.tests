db1c011ac88b5fee596b00f9764f6bdf53019278
IdosApplitools <ido.elmaleh@applitools.com>
2023-01-06 13:12:32 +0200

    java playwright initial commit

---
+++ java/playwright/overrides.js
@@ -0,0 +1,85 @@
+module.exports = {
+ 
+    // Python like
+    // fails in selenium4 only due to legacy driver being used
+    'check window after manual scroll on safari 11': { skip: true },
+    // not available in chrome > 96(will be avalable on slenium v4)
+    'check region by selector within shadow dom with vg': { skipEmit: true },
+    'check region by element within shadow dom with vg': { skipEmit: true },
+    // Feature not present in Selenium
+    'should handle check of stale element if selector is preserved': { skip: true }, // Not implemented yet
+    'should handle check of stale element in frame if selector is preserved': { skip: true }, // Not implemented yet
+    // They are testing a functionality that no longer exists for the SDK
+    'should return actual viewport size': { skip: true },
+    'should set viewport size': { skip: true },
+    'should set viewport size on edge legacy': { skip: true },
+    // Chrome emulator have minor diffs with JS sdk
+    'should not fail if scroll root is stale on android': { config: { branchName: 'universal-java' } },
+    
+    // TODO - recheck Chrome emulator flaky test (was overridden by a specific branch, maybe default will work).
+    // 'check window fully on android chrome emulator on mobile page with horizontal scroll': { skip: true },
+    // New mobile web tests which is failing on the eyes.Open() with the error
+    //
+    // com.applitools.eyes.EyesException: unknown command: Cannot call non W3C standard command while in W3C mode
+    // INFO: Detected dialect: W3C
+    // coverage.generic.CheckWindowOnMobileWebAndroid
+    // REQUEST: {"name":"EyesManager.openEyes","key":"6dba109a-d023-44ba-9466-83af1ff3a912","payload":{"manager":{"applitools-ref-id":"4e73260e-70b8-49aa-a5d2-369246ded3b3"},"driver":{"sessionId":"d723182048e34c089ced8392ed0717cd","serverUrl":"https://ondemand.saucelabs.com:443/wd/hub","capabilities":{"appActivity":"com.google.android.apps.chrome.Main","appPackage":"com.android.chrome","browserName":"chrome","databaseEnabled":false,"desired":{"sauce:options":{},"deviceName":"Google Pixel 3a XL GoogleAPI Emulator","orientation":"PORTRAIT","udid":"emulator-5554","noReset":true,"selenium:webdriver.remote.quietExceptions":false,"browserName":"chrome","noSign:noSign":true,"proxy":{"proxyAutoconfigUrl":"http://127.0.0.1:19876/pac.js","proxyType":"PAC"},"newCommandTimeout":0,"platformVersion":"10.0","platformName":"android","eventTimings":true,"maxTypingFrequency":8},"deviceApiLevel":29,"deviceManufacturer":"Google","deviceModel":"Android SDK built for x86_64","deviceName":"emulator-5554","deviceScreenDensity":400,"deviceScreenSize":"1080x2160","deviceUDID":"emulator-5554","eventTimings":true,"javascriptEnabled":true,"locationContextEnabled":false,"maxTypingFrequency":8,"networkConnectionEnabled":true,"newCommandTimeout":0,"noReset":true,"noSign:noSign":true,"orientation":"PORTRAIT","pixelRatio":2.5,"platform":"LINUX","platformName":"LINUX","platformVersion":"10","proxy":{"proxyType":"PAC","autodetect":false,"ftpProxy":null,"httpProxy":null,"noProxy":null,"sslProxy":null,"socksProxy":null,"socksVersion":null,"socksUsername":null,"socksPassword":null,"proxyAutoconfigUrl":"http://127.0.0.1:19876/pac.js"},"sauce:options":{},"selenium:webdriver.remote.quietExceptions":false,"statBarHeight":60,"takesScreenshot":true,"udid":"emulator-5554","viewportRect":{"width":1080,"top":60,"left":0,"height":1980},"warnings":{},"webStorageEnabled":false}},"config":{"debugScreenshots":{},"apiKey":"97ELuwdIiAilbeumIilysV8yY24tygCeRFFTYEBO7EfE110","serverUrl":"https://eyes.applitools.com/","appName":"Eyes Selenium SDK - Classic API","testName":"CheckWindowOnMobileWebAndroid","viewportSize":{"width":700,"height":460},"batch":{"id":"c9c27955-1d5c-4933-9987-a59fa35e0e85","name":"JAVA coverage tests","startedAt":"2022-03-13T18:32:03Z","notifyOnCompletion":false,"properties":[]},"defaultMatchSettings":{"matchLevel":"Strict","useDom":false,"enablePatterns":false,"ignoreCaret":false,"ignoreDisplacements":false},"branchName":"master","parentBranchName":"master","saveNewTests":false,"matchTimeout":0,"hideScrollbars":true,"hideCaret":true,"browsersInfo":[{"name":"chrome","width":700,"height":460}],"disabled":false}}}
+    // RESPONSE: {"name":"EyesManager.openEyes","key":"6dba109a-d023-44ba-9466-83af1ff3a912","payload":{"error":{"message":"unknown command: Cannot call non W3C standard command while in W3C mode","stack":"#0 0x56477aac9199 <unknown>\n"}}}
+    // REQUEST: {"name":"EyesManager.closeManager","key":"36cd5684-44c2-4b96-a217-803d1efc7981","payload":{"manager":{"applitools-ref-id":"4e73260e-70b8-49aa-a5d2-369246ded3b3"},"throwErr":false}}
+    // RESPONSE: {"name":"EyesManager.closeManager","key":"36cd5684-44c2-4b96-a217-803d1efc7981","payload":{"result":{"results":[],"passed":0,"unresolved":0,"failed":0,"exceptions":0,"mismatches":0,"missing":0,"matches":0}}}
+    'check window on mobile web android': { skip: true },
+    'appium iOS check fully window with scroll and pageCoverage': { skipEmit: true },
+    'appium iOS check window region with scroll and pageCoverage': { skipEmit: true },
+    // TODO verify and enable
+    'should send agentRunId': { skipEmit: true },
+    "appium iOS nav bar check regio": { skipEmit: true },
+    "Should return exception in TestResultsSummary": { skipEmit: true },
+    // TODO Failed and needs to be re-checked.
+    'appium iOS nav bar check region': { skip: true },
+    //"appium android landscape mode check window": {skip: true},
+    //"appium android landscape mode check region": {skip: true},
+    // Emitter failure
+   
+    "should override default value of fully with true": { skipEmit: true },
+    "should override default value of fully with false": { skipEmit: true },
+
+    "should send codded regions with padding": { skipEmit: true },
+    "should send codded regions with padding with vg": { skipEmit: true },
+
+    // sessionNotCreated exception
+    "check window fully on page with horizontal scroll with css stitching": { skipEmit: true },
+    "check window fully on page with horizontal scroll with scroll stitching": { skipEmit: true },
+
+    // check image
+    "check image file in png format": {skipEmit: true},
+    "check image file in jpeg format": {skipEmit: true},
+    "check image file in bmp format": {skipEmit: true},
+    "check image base64 in png format": {skipEmit: true},
+    "check image url in png format": {skipEmit: true},
+    "check image file in png format classic": {skipEmit: true},
+    "check image region": {skipEmit: true},
+    "check image region classic": {skipEmit: true},
+    "should send dom when check image": {skipEmit: true},
+
+    // devices are not available in playwright 
+    "appium iOS nav bar check region": {skipEmit: true},
+    "check window on mobile web android": {skipEmit: true},
+    "check window fully on android chrome emulator on mobile page": { skipEmit: true },
+    "check window fully on android chrome emulator on mobile page with horizontal scroll": { skipEmit: true },
+    "check window fully on android chrome emulator on desktop page": {skipEmit: true},
+    "should not fail if scroll root is stale on android": {skipEmit: true},
+    "appium android check window": {skipEmit: true},
+    "appium android check region with ignore region": {skipEmit: true},
+    "appium android check region": {skipEmit: true},
+    "appium iOS check window": {skipEmit: true},
+    "appium iOS check fully window with scroll and pageCoverage": {skipEmit: true},
+    "appium iOS check window region with scroll and pageCoverage": {skipEmit: true},
+    "appium iOS check region with ignore region": {skipEmit: true},
+    "appium iOS check region": {skipEmit: true},
+    "appium android landscape mode check window on android 7": {skipEmit: true},
+    "appium android landscape mode check window on android 10": {skipEmit: true},
+    "appium android landscape mode check region on android 7": {skipEmit: true},
+    "appium android landscape mode check region on android 10": {skipEmit: true},
+    "should capture webview when specified in check settings on android": {skipEmit: true}
+
+}
---
+++ java/playwright/parser.js
@@ -0,0 +1,296 @@
+'use strict'
+const types = require('../mapping/types')
+const selectors = require('../mapping/selectors')
+const { capitalizeFirstLetter, isEmpty } = require('../util')
+const { checkOptions } = require("../../util")
+const { CHECK_SETTINGS_HOOKS, CHECK_SETTINGS_OPTIONS, ENV_PROPERTIES } = require('../mapping/supported')
+
+
+function checkSettings(cs, native) {
+    checkOptions(cs, CHECK_SETTINGS_OPTIONS)
+    let java = `Target`
+    if (cs === undefined || isEmpty(cs)) {
+        return java + '.window()'
+    }
+    let element = ''
+    let options = ''
+    if (cs.frames === undefined && cs.region === undefined) element = '.window()'
+    else {
+        if (cs.frames) {
+            if (cs.scrollRootElement) {
+                element += `.window().scrollRootElement(${printSelector(cs.scrollRootElement)})`
+            }
+            element += frames(cs.frames)
+        }
+        if (cs.region) element += region(cs.region)
+    }
+    if (cs.webview) options += webview(cs.webview);
+    if (cs.floatingRegions) options += floatingRegions(cs.floatingRegions);
+    if (cs.accessibilityRegions) options += accessibilityRegions(cs.accessibilityRegions);
+    if (cs.ignoreRegions) options += typeRegions('ignore', cs.ignoreRegions);
+    if (cs.strictRegions) options += typeRegions('strict', cs.strictRegions);
+    if (cs.contentRegions) options += typeRegions('content', cs.contentRegions);
+    if (cs.layoutRegions) options += typeRegions('layout', cs.layoutRegions);
+    if (cs.scrollRootElement && !cs.frames) options += `.scrollRootElement(${printSelector(cs.scrollRootElement)})`;
+    if (cs.ignoreDisplacements !== undefined) options += `.ignoreDisplacements(${cs.ignoreDisplacements})`;
+    if (cs.timeout) options += `.timeout(${serialize(cs.timeout)})`;
+    if (cs.sendDom !== undefined) options += `.sendDom(${serialize(cs.sendDom)})`;
+    if (cs.matchLevel) options += `.matchLevel(MatchLevel.${cs.matchLevel.toUpperCase()})`;
+    if (cs.name) options += `.withName("${cs.name}")`;
+    if (cs.layoutBreakpoints) options += `.layoutBreakpoints(${cs.layoutBreakpoints})`;
+    if (cs.waitBeforeCapture) options += `.waitBeforeCapture(${cs.waitBeforeCapture})`;
+    if (cs.isFully === true) {
+        options += '.fully()';
+    } else if (cs.isFully === false) {
+        options += '.fully(false)';
+    }
+    if (cs.visualGridOptions) {
+        const VGOptionsKeys = Object.keys(cs.visualGridOptions);
+        const vgOptions = VGOptionsKeys.map(key => `new VisualGridOption("${key}", ${cs.visualGridOptions[key]})`).join(', ');
+        options += `.visualGridOptions(${vgOptions})`;
+    }
+    if (cs.variationGroupId) options += `.variationGroupId("${cs.variationGroupId}")`;
+    if (cs.hooks) {
+        checkOptions(cs.hooks, CHECK_SETTINGS_HOOKS);
+        if (cs.hooks.beforeCaptureScreenshot) {
+            options += `.beforeRenderScreenshotHook("${cs.hooks.beforeCaptureScreenshot}")`;
+        }
+    }
+    if (cs.pageId) {
+        options += `.pageId("${cs.pageId}")`;
+    }
+    if (cs.lazyLoad) {
+        options += lazyLoad(cs.lazyLoad);
+    }
+
+    return java + element + options;
+
+    // check settings
+
+    function webview(webview) {
+        let s;
+        switch(typeof webview) {
+            case "boolean":
+                s = `.webview(${cs.webview})`
+                break;
+            case "string":
+                s = `.webview(${JSON.stringify(cs.webview)})`
+                break;
+            default:
+                throw new Error(`webview parameter of the unimplemented type was used:  ${JSON.stringify(webview)}`);
+        }
+
+        return s;
+    }
+
+    function frames(arr) {
+        return arr.reduce((acc, val) => acc + `${frame(val)}`, '');
+    }
+
+    function frame(frame) {
+        return (!frame.isRef && frame.frame) ? `.frame(${frameSelector(frame.frame)}).scrollRootElement(${printSelector(frame.scrollRootElement)})` : `.frame(${frameSelector(frame)})`;
+    }
+
+    function frameSelector(selector) {
+        if (typeof selector === 'string' && !checkCss(selector)) {
+            return JSON.stringify(selector);
+        } else {
+            return printSelector(selector);
+        }
+
+        function checkCss(string) {
+            return (string.includes('[') && string.includes(']')) || string.includes('#');
+        }
+    }
+
+    function region(region) {
+        return `.region(${regionParameter(region)})`;
+    }
+
+    function typeRegions(type, arr) {
+        return arr.reduce((acc, val) => `${acc}.${type}(${regionParameter(val)})`, '');
+    }
+
+    function floatingRegions(arr) {
+        return arr.reduce((acc, val) => `${acc}.floating(${floating(val)})`, ``);
+    }
+
+    function floating(floating) {
+        let string;
+        string = regionParameter(floating.region);
+        string += `, ${floating.maxUpOffset}, ${floating.maxDownOffset}, ${floating.maxLeftOffset}, ${floating.maxRightOffset}`;
+        return string;
+    }
+
+    function accessibilityRegions(arr) {
+        return arr.reduce((acc, val) => `${acc}.accessibility(${accessibility(val)})`, ``);
+    }
+
+    function accessibility(val) {
+        return `${regionParameter(val.region)}, AccessibilityRegionType.${capitalizeFirstLetter(val.type)}`;
+    }
+
+    function regionParameter(region) {
+        let string;
+        switch (typeof region) {
+            case 'string':
+                string = `getPage().locator(${JSON.stringify(region)})`;
+                break;
+            case "object":
+                if (region.region) {
+                    string = regionParameter(region.region)
+                    if (region.regionId) {
+                        string += `, "${region.regionId}"`
+                    }
+                } else if (region.type) {
+                    string = native ? `getDriver().locator(${parseObject(region)})` : parseObject(region);
+                } else {
+                    string = parseObject({ value: region, type: 'Region' });
+                }
+                break;
+            case "function":
+                string = serialize(region);
+                break;
+            default:
+                throw new Error(`Region parameter of the unimplemented type was used:  ${JSON.stringify(region)}`);
+        }
+        return string
+    }
+    function lazyLoadOptions(lazyLoad) {
+        let string;
+        let llOptions;
+        const LLOptionsKeys = Object.keys(lazyLoad);
+        if (LLOptionsKeys.length == 3) {
+            llOptions = LLOptionsKeys.map(key => `${lazyLoad[key]}`).join(', ');
+            string = `new LazyLoadOptions(${llOptions})`;
+            return `.lazyLoad(${string})`
+        }
+        else {
+            llOptions = LLOptionsKeys.map(key => `${key}(${lazyLoad[key]})`).join('.');
+            string = `new LazyLoadOptions().${llOptions}`;
+            return `.lazyLoad(${string})`;
+        }
+    }
+    function lazyLoad(lazyLoad) {
+        if (isEmpty(lazyLoad))
+            return `.lazyLoad()`
+        else
+            return lazyLoadOptions(lazyLoad)
+    }
+
+}
+
+// General
+
+function java(chunks, ...values) {
+    const commands = [];
+    let code = '';
+    values.forEach((value, index) => {
+        if (typeof value === 'function' && !value.isRef) {
+            code += chunks[index];
+            commands.push(code, value);
+            code = '';
+        } else {
+            code += chunks[index] + serialize(value);
+        }
+    })
+    code += chunks[chunks.length - 1];
+    commands.push(code);
+    return commands;
+}
+
+function serialize(value) {
+    let stringified = '';
+    if (value && value.isRef) {
+        stringified = value.ref();
+    } else if (value === null) {
+        throw Error(`Null shouldn't be passed to the java code. \n ${value}`);
+    } else if (typeof value === 'object') {
+        stringified = parseObject(value);
+    } else if (typeof value === 'function') {
+        stringified = value.toString();
+    } else if (typeof value === 'undefined') {
+        throw Error(`Undefined shouldn't be passed to the java code. \n ${value}`);
+    } else {
+        stringified = JSON.stringify(value);
+    }
+    return stringified;
+}
+
+function parseObject(object) {
+    if (object.selector) {
+        return selectors[object.type](JSON.stringify(object.selector));
+    } else if (object.type) {
+        const typeBuilder = types[object.type];
+        if (typeBuilder) {
+            if (typeBuilder.isGeneric) {
+                return typeBuilder.constructor(object.value, object.generic);
+            } else {
+                return typeBuilder.constructor(object.value);
+            }
+        } else throw new Error(`Constructor wasn't implemented for the type: ${object.type}`);
+    } else return JSON.stringify(object);
+}
+
+function getter({ target, key, type }) {
+    // console.log(`target: ${target} , key: ${key}, type: ${JSON.stringify(type, null, 3)}, typeOfKey: ${typeof key}, isArray: ${Array.isArray(key)}`)
+    try {
+        if (typeof type === 'undefined') return `${target}.${key}`
+        else if (types[type.name]) return types[type.name].get(target, key)
+        else throw new Error(`Haven't implement type ${JSON.stringify(type)}`)
+    } catch (err) {
+        throw new Error(`type:=${JSON.stringify(type)}\n error:=${err}`)
+    }
+}
+
+function mapTypes(type) {
+    let mapped
+    try {
+        mapped = type ? types[type.name].name(type) : types.Element.name()
+    } catch (e) {
+        throw Error(`SDK haven't implemented support for the ${JSON.stringify(type)} \nWith error: ${e.message} \nStack:${e.stack}`)
+    }
+    return mapped
+}
+
+function wrapSelector(val) {
+    return val.selector ? val : { type: 'css', selector: val }
+}
+
+function printSelector(val) {
+    return serialize((val && val.isRef) ? val : wrapSelector(val))
+}
+
+const variable = ({ name, value, type }) => `final ${mapTypes(type)} ${name} = (${mapTypes(type)}) ${value}`
+const call = ({ target, args }) => {
+    return args.length > 0 ? `${target}(${args.map(val => JSON.stringify(val)).join(", ")})` : `${target}()`
+}
+const returnSyntax = ({ value }) => {
+    return `return ${value};`
+}
+
+function parseEnv(env) {
+    checkOptions(env, ENV_PROPERTIES)
+    let result = 'driver = buildDriver()'
+    if (env) {
+        if (env.browser) result += `.browser(${serialize(env.browser)})`
+        if (env.device) result += `.device(${serialize(env.device)})`
+        if (env.app) result += `.app(${serialize(env.app)})`
+        if (env.orientation) result += `.orientation(${serialize(env.orientation)})`
+        if (env.hasOwnProperty('headless')) result += `.headless(${serialize(env.headless)})`
+        if (env.hasOwnProperty('legacy')) result += `.legacy(${serialize(env.legacy)})`
+        if (env.hasOwnProperty('executionGrid') && env.executionGrid !== undefined) result += `.executionGrid(${serialize(env.executionGrid)})`
+    }
+    return result + '.build();'
+}
+
+module.exports = {
+    checkSettingsParser: checkSettings,
+    java: java,
+    getter: getter,
+    variable: variable,
+    call: call,
+    returnSyntax: returnSyntax,
+    wrapSelector: wrapSelector,
+    parseEnv: parseEnv
+}
