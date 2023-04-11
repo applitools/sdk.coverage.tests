ca9d5081f039a2640a562ef7f2e2bda2a40c248a
kyrylo <kyrylo.onufriiev@applitools.com>
2023-04-11 10:41:43 +0300

    [js] add sleep before fetching test results

--- js/emitter.js
+++ js/emitter.js
@@ -269,7 +269,10 @@ module.exports = function(tracker, test) {
       return addCommand(js`await new Promise(r => setTimeout(r, ${milliseconds}))`)
     },
     getTestInfo(result) {
-      return addCommand(js`await getTestInfo(${result}, eyes.configuration && eyes.configuration.apiKey)`)
+      return addCommand(js`
+        await new Promise(done => setTimeout(done, 5000))
+        await getTestInfo(${result}, eyes.configuration && eyes.configuration.apiKey)
+      `)
     },
     getDom(result, domId) {
       return addCommand(js`await getTestDom(${result}, ${domId})`).methods({
