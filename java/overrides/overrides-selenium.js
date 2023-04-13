0a1f61a8eb538a26a56936a50b83d86915a629f8
IdosApplitools <ido.elmaleh@applitools.com>
2023-04-13 17:21:40 +0300

    [java] support removeDuplicateTests

--- java/overrides/overrides-selenium.js
+++ java/overrides/overrides-selenium.js
@@ -57,11 +57,4 @@ module.exports = {
     "check image region": {skipEmit: true},
     "check image region classic": {skipEmit: true},
     "should send dom when check image": {skipEmit: true},
-
-    // Support removing duplicates:
-    // TODO: update the emitter to support passing the new runner option `removeDuplicateTestsPerBatch`
-    "should support removal of duplicate test results with classic": {skipEmit: true},
-    "should support removal of duplicate test results with ufg": {skipEmit: true},
-    "should skip removal of duplicate test results when baseline name used with classic": {skipEmit: true},
-    "should skip removal of duplicate test results when baseline name used with ufg": {skipEmit: true},
 }
