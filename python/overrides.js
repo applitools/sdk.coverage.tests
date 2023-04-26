f8e5f1b137ccfd05b5e1e76c76d0bea3a8e07330
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-04-26 12:45:15 -0700

    [py] unskip more passing tests (#190)

--- python/overrides.js
+++ python/overrides.js
@@ -1,13 +1,4 @@
 module.exports = {
     // fails on chrome>=96
     'check region by element within shadow dom with vg': {skip: true},
-    
-    // 100% broken since Apr 10, 2023, was flaky 5 days before
-    'should send custom batch properties': {skip: true},
-
-    // Stale element are not handled by python binding
-    'should handle check of stale element if selector is preserved': {skip: true},
-    'should handle check of stale element in frame if selector is preserved': {skip: true},
-
-    "Should return exception in TestResultsSummary": {skipEmit: true},
 }
