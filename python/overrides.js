a7420515dfffa18ae8af911d6df46d99e60bd81e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-04-25 20:41:17 -0700

    [py] Unskip stale elements tests, they are now skipped by generated tests configuration

--- python/overrides.js
+++ python/overrides.js
@@ -1,8 +1,4 @@
 module.exports = {
     // fails on chrome>=96
     'check region by element within shadow dom with vg': {skip: true},
-    
-    // Stale element are not handled by python binding
-    'should handle check of stale element if selector is preserved': {skip: true},
-    'should handle check of stale element in frame if selector is preserved': {skip: true},
 }
