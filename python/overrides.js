e258c1c33ed37a99c235f94da8b3c3d12eacff4c
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-06-28 18:52:13 -0700

    [py] skip appium iOS check window because it has ios13 baseline and ios13 sauce emulators are extremely unstable now

--- python/overrides.js
+++ python/overrides.js
@@ -1,4 +1,6 @@
 module.exports = {
     // fails on chrome>=96
     'check region by element within shadow dom with vg': {skip: true},
+    // the baselines for this test are ios13 which is very unstable on sauce now
+    'appium iOS check window': {skip: true},
 }
