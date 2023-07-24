813b108ddd38e817b1b0aeecb473624579e8b4bf
kyrylo <kyrylo.onufriiev@applitools.com>
2023-07-24 10:55:09 +0300

    chore(js): add esm config

---
+++ js/config.mjs
@@ -0,0 +1,21 @@
+export default {
+  output: './test/generic',
+  ext: '.spec.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/emitter.js',
+  overrides: [
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/js-overrides.js',
+    ...(process.env.APPLITOOLS_TEST_REMOTE === 'eg' ? ['https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/eg.overrides.js'] : []),
+  ],
+  template:
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/template.hbs',
+  tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
+  fixtures: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/fixtures/fixtures.zip',
+  format: {
+    parser: 'babel',
+    singleQuote: true,
+    semi: false,
+    bracketSpacing: false,
+    trailingComma: 'es5',
+  },
+}
