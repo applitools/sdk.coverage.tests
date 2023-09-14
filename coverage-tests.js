9b6789ed01387765ccab65b8d390efddee1ef7bf
Itai Bar-Haim <itai.barhaim@applitools.com>
2023-09-14 10:59:14 +0300

    added all browsers test for VG

--- coverage-tests.js
+++ coverage-tests.js
@@ -106,6 +106,36 @@ test('check window', {
   },
 })
 
+test('check window with vg on all browsers', {
+  page: 'Default',
+  vg: true,
+  config: {
+    browsersInfo: [
+      {name: 'chrome', width: 1000, height: 800},
+      {name: 'chrome-one-version-back', width: 1000, height: 800},
+      {name: 'chrome-two-versions-back', width: 1000, height: 800},
+      {name: 'firefox', width: 1000, height: 800},
+      {name: 'firefox-one-version-back', width: 1000, height: 800},
+      {name: 'firefox-two-versions-back', width: 1000, height: 800},
+      {name: 'safari', width: 1000, height: 800},
+      {name: 'safari-one-version-back', width: 1000, height: 800},
+      {name: 'safari-two-versions-back', width: 1000, height: 800},
+      {name: 'safari-earlyaccess', width: 1000, height: 800},
+      {name: 'ie10', width: 1000, height: 800},
+      {name: 'ie11', width: 1000, height: 800},
+      {name: 'edgelegacy', width: 1000, height: 800},
+      {name: 'edgechromium', width: 1000, height: 800},
+      {name: 'edgechromium-one-version-back', width: 1000, height: 800},
+      {name: 'edgechromium-two-versions-back', width: 1000, height: 800},
+    ],
+  },
+  test({eyes}) {
+    eyes.open({appName: 'Applitools Eyes SDK'})
+    eyes.check()
+    eyes.close()
+  }
+})
+
 test('check window fully', {
   page: 'Default',
   variants: {
